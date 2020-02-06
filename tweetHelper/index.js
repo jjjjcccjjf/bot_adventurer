8c199ad8fbd7b05514ca1adcf4e83b7c9a2ea20e
Lorenzo Salamante <lsalamante@myoptimind.com>
2020-02-06 18:08:28 +0800

    Add more stuff

--- tweetHelper/index.js
+++ tweetHelper/index.js
@@ -6,25 +6,50 @@ let monsters = [];
 let npcs = [];
 let actions = [];
 let locations = []
+let ambient = []
 
 locations = [
 	{ 
 		name: "room", 
 		descriptions: ["dim", "brightly lit", "menacing", "dangerous", "normal", "common", "damp", "relatively safe", "foggy", "misty", "clear", "bright", "fine", "pretty regular", "eerie", "creepy", "dark", "safe", "creaking", "alright", "funny", "pretty normal", "normal", "normal", "normal", "normal", "normal"] 
-	}
+	},
+	{ 
+		name: "cliff", 
+		descriptions: ["steep", "high", "dark", "foreboding", "scary", "mesmerizing", "stunning"] 
+	},
 ];
 
 actions = [
 	{ 
 		name: "movement_between_rooms", 
 		descriptions: ["proceeded", "entered", "moved", "went", "climbed", "ran", "walked", "strolled"] 
-	}
-	// ,
+	},
+	{ 
+		name: "wait", 
+		descriptions: ["waited", "stayed", "stayed put", "marvelled the scenery", "inhaled a lungful of air"] 
+	},
 	// { 
-	// 	name: "wait", 
-	// 	descriptions: ["waited", "stayed", "stayed put", "did nothing"] 
+	// 	name: "did_nothing", 
+	// 	descriptions: [] 
+	// },
+	// { 
+	// 	name: "feel_possesive", 
+	// 	descriptions: ["eyelids felt heavy"] 
+	// },
+	// { 
+	// 	name: "feel", 
+	// 	descriptions: ["felt cold", "shivered", "felt comfortable", "felt energized", "felt lonely", "felt fine", "felt a shiver ran down your spine", "felt at ease", "felt well-rested"] 
 	// }
 ];
+
+ambient = [
+	{
+		name: "smell", 
+		descriptions: ["the smell of fish"],
+		helper: ["wafted through your senses", "pervaded your nostrils"]
+	}
+]
+
 
 let resources = {
 	locations: locations,
@@ -32,7 +57,8 @@ let resources = {
 	items: items,
 	monsters: monsters,
 	npcs: npcs,
-	actions: actions
+	actions: actions,
+	ambient: ambient
 }
 
 /**
@@ -47,13 +73,25 @@ function dispatch(resources) {
 	let action = actions[Math.floor(Math.random() * actions.length)] // draw random action
 	let location = locations[Math.floor(Math.random() * locations.length)] // draw random location
 
-	let phrase = "you did nothing.";
+	let phrase = ""
 
 	switch(action.name) {
+
 		case 'movement_between_rooms':
-		let action_p = action.descriptions[Math.floor(Math.random() * action.descriptions.length)]; // draw random action phrase // example: proceeded
-		let location_p = location.descriptions[Math.floor(Math.random() * location.descriptions.length)] + " " + location.name // draw random location plus adjective // ex. brightly lit room
-		phrase = generateMovementPhrase(action_p, location_p)
+		let action_mbr = action.descriptions[Math.floor(Math.random() * action.descriptions.length)]; // draw random action phrase // example: proceeded
+		let location_mbr = location.descriptions[Math.floor(Math.random() * location.descriptions.length)] + " " + location.name // draw random location plus adjective // ex. brightly lit room
+		phrase = generateMovementPhrase(action_mbr, location_mbr)
+		break
+
+        case 'wait':
+		let action_w = action.descriptions[Math.floor(Math.random() * action.descriptions.length)]; // draw random action phrase // example: proceeded
+		let location_w = location.descriptions[Math.floor(Math.random() * location.descriptions.length)] + " " + location.name // draw random location plus adjective // ex. brightly lit room
+		phrase = generateWaitPhrase(action_w, location_w)
+		break
+
+		case 'did_nothing':
+		default:
+		phrase = "you did nothing."
 		break
 	}
 
@@ -67,7 +105,11 @@ function dispatch(resources) {
  * @return {[type]}   [description]
  */
 function generateMovementPhrase(a, l) {
-	return "you " + a + " to a " + l + "."
+	return "you " + a + " to the " + l + "."
+}
+
+function generateWaitPhrase(a, l) {
+	return "you " + a + " at the " + l + "."
 }
 
 
